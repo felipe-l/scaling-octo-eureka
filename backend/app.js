47da50f1502c71635f5321711eb73c801893dded
bit <pipecarnal123@gmail.com>
2024-10-08 23:49:23 +0000

    sample change

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Docker! This is a sample change! 12345');
+    res.send('Hello, Docker! This is a sample change! Does it work?');
 });
 
 app.get('/secret', (req, res) => {
