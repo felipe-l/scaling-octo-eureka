3d7dc4cd6917fbe5156b5cccc65b97648c3f4038
bit <pipecarnal123@gmail.com>
2024-10-08 23:19:10 +0000

    changed endpoint response

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Docker! This is a sample change!');
+    res.send('Hello, Docker! This is a sample change! 123');
 });
 
 app.get('/secret', (req, res) => {
